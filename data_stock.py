71242bdaf08ed93501d2ca75a1781e2b454f324b
djarecka <djarecka@gmail.com>
2015-09-07 14:22:47 -0600

    testing quandl library

--- data_stock.py
+++ data_stock.py
@@ -1,10 +1,10 @@
-import Quandl
+#import Quandl
 import pandas
-import pdb
+#import pdb
 
 #TODO ticker symbol i miesiace
 def monthly_data():
-    mydata = Quandl.get("WIKI/GOOG", trim_start="2014-08-07", trim_end="2015-09-07")
+   # mydata = Quandl.get("WIKI/GOOG", trim_start="2014-08-07", trim_end="2015-09-07")
     
     pdb.set_trace()
 
